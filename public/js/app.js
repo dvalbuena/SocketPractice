545bea5d39725b6b54e45c06211401927bf18a52
dvalbuena <denice_valbuena@csu.fullerton.edu>
2015-05-06 04:17:19 +0000

    it works

--- public/js/app.js
+++ public/js/app.js
@@ -6,27 +6,51 @@ var turn = red;
 var socket = io();
 //socket.emit('assignToken', turn);
 
-
-
-socket.on('move', function(moveJSON) {
-    console.log("Received move event", moveJSON);
+function updateGrid(moveJSON, isBroadcast) {
     if(moveJSON.valid) {
         console.log("#" + moveJSON.move);
         if(moveJSON.turn === red) {
             $("#" + moveJSON.move).addClass("piece").addClass("red");
-            turn = blue;
+            if(isBroadcast === true) {
+                turn = blue;
+            }
         } else {
             $("#" + moveJSON.move).addClass("piece").addClass("blue");
-            turn = red;
+            if(isBroadcast === true) {
+                turn = red;
+            }
         }
-    } else {
-        window.alert("Invalid Move");
+    } 
+}
+
+socket.on('move', function(moveJSON) {
+    console.log("Received move event", moveJSON);
+    if(moveJSON.valid) {
+        updateGrid(moveJSON, true);   
     }
 });
 
+socket.on('winner', function(turn) {
+  window.alert("Player " + turn + "wins");
+});
+
+socket.on('move2', function(moveJSON) {
+    
+    if(moveJSON.valid) {      
+        updateGrid(moveJSON, false);
+    } else {
+        window.alert("It is not your turn");
+    }
+    
 
 
-//socket.on()
+});
+
+//include a message to tell the user that the room is full
+socket.on('denied', function () {
+    //disable the ability to click on the grid
+    $("div.c4-control div.piece").style.pointerEvents = 'none';
+});
 
 $("div.c4-control div.piece").hover(
     // hover over
@@ -42,7 +66,8 @@ $("div.c4-control div.piece").hover(
         $(this).removeClass("red").removeClass("blue");
     }
 );
-
+//var possiblemoves = [0.1,2,3,5,6];
+//var setTime = 7000;
 $("div.c4-control div.piece").click(function () {
     var column = parseInt($(this).attr("id"));
     /* for (var row = 6; row > 0; row--) {
@@ -64,9 +89,26 @@ $("div.c4-control div.piece").click(function () {
             break;
         }
     } */
-    
+  //  setTime = 7000;
     $(this).removeClass("red").removeClass("blue");
     var moveJSON = {'color': turn, 'column':column };
     // alert("click " + JSON.stringify(moveJSON));
     socket.emit('validateMove', moveJSON);
 });
+
+//if no button is click set start the timer countdown
+
+/*setInterval(computermove,setTime);
+
+function computermove(){
+var compmove = Math.floor((Math.random()* possiblemoves.length));
+var compChoice = possiblemoves[compmove];
+console.log("this is random comp move " + compChoice);
+//remove choice from the array
+
+    $(this).removeClass("red").removeClass("blue");
+    var moveJSON = {'color': turn, 'column':compChoice };
+    // alert("click " + JSON.stringify(moveJSON));
+    socket.emit('validateMove', moveJSON);
+    //possiblemoves.splice(compmove,1);
+};*/
--- server.js
+++ server.js
@@ -10,9 +10,6 @@ var io = require('socket.io')(http);
 var mongoose = require("mongoose");
 
 
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
@@ -26,7 +23,8 @@ app.use('/js', express.static(__dirname + '/public/js'));
 // enum
 var red = 1, blue = 2, empty = 3;
 var turn = red;
-
+//keep track of moves 
+var columns = [[], [], [], [], [], [], []];
 var height = [5, 5, 5, 5, 5, 5, 5];
 
 //set up board for connect 4 
@@ -43,28 +41,45 @@ var grid = [[empty,empty,empty,empty,empty,empty],
 //console.log(grid[0][5]);
 //console.log(grid[6][5]);
 var playerturn = {};
-var maxUsers =0;
+var users = [];
+var sers = 0;
+var id = 0;
+var maxUsers = 0;
+
 io.on('connection', function(socket) {
-    console.log('a user connected');
     
+    console.log('a user connected');
+    maxUsers++;
+/*    socket.username = id;
+    users[id] = id;
+    console.log(users);
+    id++;*/
     
     socket.on('disconnect', function() {
         console.log('user disconnected');
+        delete users[socket.username];
+        id--;
+        maxUsers--;
     });
+
+    //call the event to disable playing for 3rd, 4th, ... users
+    if(maxUsers > 2) {
+        socket.emit('denied');
+    }
 
 	//assign token for user blue or red
 /*	socket.on('assignToken', function(turn){
 		socket.turn = turn;
 		//add user to the list of turn
 		playerturn[turn] = turn;
-		++maxUsers;
+		++sers;
 		
 	});
 
 	//echo the a player has been assigned a token
 	socket.broadcast.emit('user joined', {
 		playerturn: socket.turn,
-		maxUsers: maxUsers
+		sers: sers
 	})*/
 
 
@@ -72,7 +87,9 @@ io.on('connection', function(socket) {
     	console.log(JSON.stringify(moveJSON));
     	
     	if(moveJSON.color === turn && height[moveJSON.column] < 6) {
+    		//x is the column
     		var x = moveJSON.column;
+    		//y is row
     		var y = height[moveJSON.column];
     		var move = x.toString() + y.toString();
     		
@@ -82,9 +99,10 @@ io.on('connection', function(socket) {
     		
     		var responseJSON = {'turn':turn, 'valid':true, 'move':move};
     		console.log("RESPONSE:" + JSON.stringify(responseJSON));
-    		
+    		//check move status;
+			moveStatus(turn,x,y);
     		socket.broadcast.emit('move', responseJSON);
-    		socket.emit('move', responseJSON);
+    		socket.emit('move2', responseJSON);
 
     		height[moveJSON.column]--;
     		
@@ -99,11 +117,71 @@ io.on('connection', function(socket) {
     		console.log("RESPONSE:" + JSON.stringify(responseJSON));
     		
     		socket.broadcast.emit('move', responseJSON);
-            socket.emit('move', responseJSON);
+            socket.emit('move2', responseJSON);
     	}
     });
 });
 
+//logic of game source http://www.codecademy.com/Smwaters/codebits/Uln4W/edit
+//movestatus
+var moveStatus = function(turn,col,row){
+	//var lengthCol = columns[5-row].length;
+	//if(lengthCol >= 6) return false;
+	columns[col].push(turn);
+	if(winner(turn,col,row)){
+		console.log("player " + turn + " wins");
+		io.emit('winner',turn);
+	}
+	if(columns[col].length === 6){
+		checkForDraw();
+	}
+}
+
+
+//locate the move made
+var locate = function(turn, col, row) {
+	
+//	row = 5-row1;
+	console.log("this is col " + col);
+	console.log("this is row " + row);
+    if((col < 0) || (col > 6)) return false;
+    if((row < 0) || (row > 6 - 1)) return false;
+    if(columns[col].length < (row + 1)) return false;
+    return (columns[col][row] === turn);
+};
+
+//check for win
+var winner = function(turn, col, row1) {
+	console.log("checking for win");
+	var row = 5-row1;
+    if(!locate(turn, col, row)) return false;
+    var direct = [[1,0], [1,1], [0,1], [1,-1]];
+    var matches = 0;
+    for(var i = 0; i < 4; i++) {
+        for(var j = 1; ; j++)
+            if(locate(turn, col+j*direct[i][0], row+j*direct[i][1])){
+                matches++;
+   				console.log("checking for matches" + matches);
+            }
+            else break;
+        for(var j = 1; ; j++)
+            if(locate(turn, col-j*direct[i][0], row-j*direct[i][1]))
+                matches++;
+            else break;
+        if(matches >= 3) return true;
+        matches = 0;
+    }
+    return false;
+};
+
+var checkForDraw = function() {
+    for(var i = 0; i < columns.length; i++)
+        if(columns[i].length < 6)
+            return;
+     //window.alert("Draw");
+    // window.alert("win");
+    //playing = false;
+};
 
 // Create user session
 /*app.use(session({
@@ -123,12 +201,10 @@ app.use(function(req, res, next) {
 });*/
 
 
-var server = http.listen(process.env.PORT, process.env.IP, function() {
+/*var server = http.listen(3000, function() {
 	console.log('Cloud9');
-});
+});*/
 
-/*
-var server = app.listen(3000, function() {
-	console.log('Listening on port 3000');
+var server = http.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Cloud9');
 });
-*/
